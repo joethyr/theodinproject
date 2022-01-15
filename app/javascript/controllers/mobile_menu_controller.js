8c8d4ff0193b562c84c0c665e33e4a4d84fd8dc2
Kevin Mulhern <kevinmulhern23@gmail.com>
2022-01-15 16:08:55 +0000

    WIP: Get click outside working on mobile nav

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,23 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
-import { useClickOutside } from 'stimulus-use'
 
 export default class MobileMenuController extends Controller {
   static targets = ['menu'];
 
-  connect() {
-    useClickOutside(this)
+  open() {
+    this.menuTarget.classList.remove('hidden');
   }
 
-  toggle(event) {
-    event.preventDefault()
-
-    console.log('toggle');
-    this.menuTarget.classList.toggle('hidden');
-  }
-
-  clickOutside(event) {
-    if(!this.menuTarget.classList.contains('hidden')) {
-      this.menuTarget.classList.add('hidden');
-    }
+  close() {
+    this.menuTarget.classList.add('hidden');
   }
 }
